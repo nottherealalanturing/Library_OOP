3007416e5ba7d2df7844ae6adbbcb367261f6f04
Assad Isah <nottherealalanturing@gmail.com>
2022-07-18 20:28:16 +0100

    feat: add Student class

---
+++ Student.rb
@@ -0,0 +1,12 @@
+require "./Person.rb"
+
+class Student < Person
+    def initialize(name="unknown", age, parent_permision=true, classroom)
+        super(name, age, parent_permision)
+        @classroom = classroom
+    end
+
+    def play_hooky
+        "¯\(ツ)/¯"
+    end
+end
