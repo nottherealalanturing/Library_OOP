4e94a436b10aac4564c30fa73043816a053c8477
Assad Isah <nottherealalanturing@gmail.com>
2022-07-21 02:42:39 +0100

    fix: implement changes from review

--- book.rb
+++ book.rb
@@ -6,4 +6,8 @@ class Book
     @book = book
     @rentals = []
   end
+
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+  end
 end
--- person.rb
+++ person.rb
@@ -23,6 +23,10 @@ class Person < Nameable
     name
   end
 
+  def add_rental(date, book)
+    Rental.new(date, book, self)
+  end
+
   private
 
   def of_age?
