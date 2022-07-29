920c58dc73262971eb4a217300f86c4a01b8b720
Assad Isah <nottherealalanturing@gmail.com>
2022-07-29 20:24:57 +0100

    test: add test for Teacher class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,20 @@
+require_relative '../teacher'
+
+RSpec.describe Teacher do
+  before(:all) do
+    @teacher = Teacher.new('Mathematics', 20, 'FakeTeacher')
+  end
+
+  context 'when a new Teacher is created' do
+
+    it 'should be an instance teacher class' do
+        expect(@teacher).to be_instance_of Teacher
+    end
+  
+    it "should have an can_use_services method" do
+        expect(@teacher).to respond_to(:can_use_services?)
+     end
+
+  end
+end
+
